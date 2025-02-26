faad617c655c82f88c294d8eb0bfeb1f22ab6708
Phạm Bá Nguyên Trung <96678237+NguyenTrung1905@users.noreply.github.com>
2025-02-26 13:13:10 +0700

    Create-object and connect-database with Entity FrameWork Scaffold

---
+++ 2025.Services.AuthAPI/Core/Entities/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2025.Services.AuthAPI.Core.Entities;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string? FullName { get; set; }
+
+    public string UserName { get; set; }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+
+    public string PasswordSalt { get; set; }
+
+    public string? Title { get; set; }
+
+    public int Status { get; set; }
+
+    public int? Role { get; set; }
+
+    public DateTime CreatedOn { get; set; }
+
+    public DateTime? ModifiedOn { get; set; }
+
+    public bool Delete { get; set; }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 2025.TaskManager
+# 2025.sql
+PassWord PostgresSQL:12345
+Scaffold-DbContext "Server=localhost;Port=5432;Database=identity;User Id=postgres;Password=12345" -Provider "Npgsql.EntityFrameworkCore.PostgreSQL" -OutputDir "Core\Entities" -ContextDir ".\Core" -Context "IdentityContext" -Force
